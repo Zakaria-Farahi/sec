70435c8f20c22f5c27dbbe031124e3f155d54815
Zakaria-Farahi <zakariafarahi521@gmail.com>
2024-12-29 21:49:25 +0100

    change favicon 2

--- src/constants/icon.ts
+++ src/constants/icon.ts
@@ -5,7 +5,7 @@ export const defaultFavicons: Favicon[] = [
     src: '/favicon/favicon-light-32.png',
     theme: 'light',
     sizes: '32x32',
-  },
+  },*/
   {
     src: '/favicon/favicon-light-128.png',
     theme: 'light',
@@ -20,30 +20,25 @@ export const defaultFavicons: Favicon[] = [
     src: '/favicon/favicon-light-192.png',
     theme: 'light',
     sizes: '192x192',
-  },
+  },/*
   {
     src: '/favicon/favicon-dark-32.png',
     theme: 'dark',
     sizes: '32x32',
-  },
+  },*/
   {
-    src: '/favicon/favicon-dark-128.png',
+    src: '/favicon/favicon-light-128.png',
     theme: 'dark',
     sizes: '128x128',
   },
   {
-    src: '/favicon/favicon-dark-180.png',
+    src: '/favicon/favicon-light-180.png',
     theme: 'dark',
     sizes: '180x180',
   },
   {
-    src: '/favicon/favicon-dark-192.png',
+    src: '/favicon/favicon-light-192.png',
     theme: 'dark',
     sizes: '192x192',
-  },*/
-  {
-    src: '/favicon/nmap-logo.svg',
-    //theme: 'dark',
-    //sizes: '192x192',
   },
 ]
